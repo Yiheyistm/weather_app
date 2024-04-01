5194710be19139cdd3c835354ef978273ebff79f
yiheyistm <yiheyisyt23@gmail.com>
2024-04-02 00:34:31 +0300

    initialize the bloc and creating of .env file to store the Weather api key

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -1,13 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:weather/weather.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   WeatherBloc() : super(WeatherInitial()) {
-    on<WeatherEvent>((event, emit) {
-      // TODO: implement event handler
+    on<FetchWeather>((event, emit) {
+      emit(WeatherLoading());
+      try {
+        WeatherFactory weatherFactory =
+            WeatherFactory('Api_key', language: Language.ENGLISH);
+        Weather weather = 
+        emit(WeatherSuccess(weather: weather));
+      } catch (e) {
+        emit(WeatherFailure());
+      }
     });
   }
 }
--- lib/bloc/weather_event.dart
+++ lib/bloc/weather_event.dart
@@ -6,3 +6,4 @@ sealed class WeatherEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+final class FetchWeather extends WeatherEvent{}
